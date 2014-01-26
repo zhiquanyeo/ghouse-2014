134e1a234ce8f482235f4ef1533a7b5cf39efe3d
Robotics <Robotics@10.24.14.231>
2014-01-26 14:28:32 -0500

    Some additional components to the robot class

--- src/edu/wpi/first/wpilibj/templates/GHouse2014Robot.java
+++ src/edu/wpi/first/wpilibj/templates/GHouse2014Robot.java
@@ -9,8 +9,10 @@ package edu.wpi.first.wpilibj.templates;
 
 
 import edu.ghouse.drivesystem.MultiCANJaguar;
+import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.SimpleRobot;
+import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.SpeedController;
 import edu.wpi.first.wpilibj.can.CANTimeoutException;
 
@@ -22,10 +24,26 @@ import edu.wpi.first.wpilibj.can.CANTimeoutException;
  * directory.
  */
 public class GHouse2014Robot extends SimpleRobot {
+    /*** Constants ***/
+    //==== General Constants ====
+    private final int PRESSURE_SWITCH_CH = 1; //Digital IO
+    private final int COMPRESSOR_RELAY_CH = 2; //Digital IO
+    
+    //==== Drivetrain constants ====
+    //Speed change solenoid channels
+    private final int LEFT_SPEED_CHANGE_CH = 1; //Pneumatics Slot
+    private final int RIGHT_SPEED_CHANGE_CH = 2; //Pneumatics Slot
+    
+    /*** General Components ***/
+    private Compressor compressor = new Compressor(PRESSURE_SWITCH_CH, COMPRESSOR_RELAY_CH);
+    
+    /*** Drivetrain Components ***/
     private int leftControllerChannels[] = {11, 12, 13};
     private int rightControllerChannels[] = {14, 15, 16};
     private MultiCANJaguar leftController, rightController;
     private RobotDrive chassis;
+    private Solenoid leftSpeedChangeSolenoid = new Solenoid(LEFT_SPEED_CHANGE_CH);
+    private Solenoid rightSpeedChangeSolenoid = new Solenoid(RIGHT_SPEED_CHANGE_CH);
 
     public GHouse2014Robot() {
         try {
@@ -33,6 +51,10 @@ public class GHouse2014Robot extends SimpleRobot {
             rightController = new MultiCANJaguar(rightControllerChannels);
             
             chassis = new RobotDrive(leftController, rightController);
+            
+            //Setup routines
+            //Start the compressor
+            compressor.start();
             
         } catch (CANTimeoutException e) {
             System.out.println("WARNING: CANTimeoutException: " + e.getMessage());
@@ -43,14 +65,24 @@ public class GHouse2014Robot extends SimpleRobot {
      * This function is called once each time the robot enters autonomous mode.
      */
     public void autonomous() {
-        
+        while (isEnabled() && isAutonomous()) {
+            
+        }
     }
 
     /**
      * This function is called once each time the robot enters operator control.
      */
     public void operatorControl() {
-
+        //TODO Do any initial resetting here
+        
+        //Default loop
+        while (isEnabled() && isOperatorControl()) {
+            //1) Sense
+            //TODO: Take in any sensor data that we need
+            
+            //2) Act
+        }
     }
     
     /**
