088b768c7b1a9f395352bbe35dab63ea550a601d
Developer <Developer@10.24.14.185>
2014-02-09 14:14:54 -0300

    Added new subsytem modules for the feed and scissor mechanisms

---
+++ src/edu/ghouse/robot2014/FeedMechanism.java
@@ -0,0 +1,93 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package edu.ghouse.robot2014;
+
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.Victor;
+
+/**
+ *
+ * @author Developer
+ */
+public class FeedMechanism {
+    private DoubleSolenoid solenoid;
+    private Victor motor;
+    private DigitalInput insideSensor;
+    private DigitalInput outsideSensor;
+    
+    //State variables
+    private boolean armUp = true;
+    private boolean armInTransit = false;
+    
+    //to keep track of when we should shut off the  solenoid
+    private long stopRequestedTime;
+    private boolean stopRequested = false;
+    private final int SHUTOFF_TIME = 2000; //Shutoff time in ms
+    
+    public FeedMechanism(DoubleSolenoid solenoid, Victor motor, 
+                        DigitalInput insideSensor, DigitalInput outsideSensor) {
+        this.solenoid = solenoid;
+        this.motor = motor;
+        this.insideSensor = insideSensor;
+        this.outsideSensor = outsideSensor;
+    
+        //we can turn off safety for the motor since it's not a drive
+        this.motor.setSafetyEnabled(false);
+    }
+    
+    public void raiseArm() {
+        solenoid.set(DoubleSolenoid.Value.kForward);
+        stopRequested = true;
+        stopRequestedTime = System.currentTimeMillis();
+    }
+    
+    public void lowerArm() {
+        solenoid.set(DoubleSolenoid.Value.kReverse);
+        stopRequested = true;
+        stopRequestedTime = System.currentTimeMillis();
+    }
+    
+    /**
+     * This is where the magic happens
+     * We have to keep track of the arm state
+     * and update accordingly
+     */
+    public void updateState() {
+        //Handle any solenoid stop orders first
+        if (stopRequested) {
+            if (System.currentTimeMillis() - stopRequestedTime > SHUTOFF_TIME) {
+                stopRequested = false;
+                this.solenoid.set(DoubleSolenoid.Value.kOff);
+            }
+        }
+        
+        //Check the state of the arm
+        if (insideSensor.get() && !outsideSensor.get()) {
+            this.armUp = false;
+            this.armInTransit = false;
+            this.motor.set(1);
+        }
+        else if (!insideSensor.get() && outsideSensor.get()) {
+            this.armUp = true;
+            this.armInTransit = false;
+            this.motor.stopMotor();
+        }
+        else {
+            this.armInTransit = true;
+            this.motor.stopMotor();
+        }
+    }
+    
+    public boolean isArmUp() {
+        return this.armUp;
+    }
+    
+    public boolean isArmInTransit() {
+        return this.armInTransit;
+    }
+}
---
+++ src/edu/ghouse/robot2014/ScissorMechanism.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package edu.ghouse.robot2014;
+
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+
+/**
+ *
+ * @author Developer
+ */
+public class ScissorMechanism {
+    private DoubleSolenoid solenoid;
+    private DigitalInput insideSensor;
+    private DigitalInput outsideSensor;
+    
+    //State variables
+    private boolean scissorUp = false;
+    private boolean scissorInTransit = false;
+    
+    //Keep track of when we should shut off
+    private long stopRequestedTime;
+    private boolean stopRequested = false;
+    private final int SHUTOFF_TIME = 3000; //ms
+    
+    public ScissorMechanism(DoubleSolenoid solenoid, DigitalInput insideSensor, DigitalInput outsideSensor) {
+        this.solenoid = solenoid;
+        this.insideSensor = insideSensor;
+        this.outsideSensor = outsideSensor;
+    }
+    
+    public void raiseScissor() {
+        
+    }
+    
+    public void lowerScissor() {
+        
+    }
+    
+    public void updateState() {
+        if (stopRequested) {
+            if (System.currentTimeMillis() - stopRequestedTime > SHUTOFF_TIME) {
+                stopRequested = false;
+                this.solenoid.set(DoubleSolenoid.Value.kOff);
+            }
+        }
+        
+        if (insideSensor.get() && !outsideSensor.get()) {
+            this.scissorUp = true;
+            this.scissorInTransit = false;
+        }
+        else if (!insideSensor.get() && outsideSensor.get()) {
+            this.scissorUp = false;
+            this.scissorInTransit = false;
+        }
+        else {
+            this.scissorInTransit = true;
+        }
+    }
+    
+    public boolean isScissorUp() {
+        return this.scissorUp;
+    }
+    
+    public boolean isScissorInTransit() {
+        return this.scissorInTransit;
+    }
+}
